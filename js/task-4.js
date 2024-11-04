ab3217b413d5fe47e2c9fd2188d56d42087e2173
Daryna <buriak.daryna@gmail.com>
2024-11-04 20:59:58 +0200

    Add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -6,11 +6,20 @@ Chile — 250 кредитів
 Australia — 170 кредитів
 Jamaica — 120 кредитів
 Зі списку видно, що доставка можлива не скрізь. Якщо зазначена країна відсутня у списку, то функція повинна повернути рядок "Sorry, there is no delivery to your country".
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.*/
 
+function getShippingCost(country) {
+    switch (country) {
+        case "China": {return `Shipping to China will cost 100 credits`;break;}
+        case "Chile": {return `Shipping to Chile will cost 250 credits`;break;}
+        case "Australia": {return `Shipping to Australia will cost 170 credits`;break;}
+        case "Jamaica": {return `Shipping to Jamaica will cost 120 credits`;break;};
+        default: {return "Sorry, there is no delivery to your country"};
+    }
+};
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
 console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
 console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"*/
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
