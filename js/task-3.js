68c2a3eac7414d1bbe8fc16a2ed9f54d85492ef7
Daryna <buriak.daryna@gmail.com>
2024-11-04 21:23:09 +0200

    Add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 /*Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
 Доповни код функції таким чином, що:
 Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-Якщо в рядку відсутні заборонені слова, функція повертає буль false
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+Якщо в рядку відсутні заборонені слова, функція повертає буль false*/
+
+function checkForSpam(message) {
+    if (message.toLowerCase().includes("spam")) {return true}
+    else if (message.toLowerCase().includes("sale")) {return true}
+    else {return false};
+};
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
@@ -10,4 +15,4 @@ console.log(checkForSpam("Get best sale offers now!")); // true
 console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true*/
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
